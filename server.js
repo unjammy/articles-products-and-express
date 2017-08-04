f7b26a06e40db426b08e26d45b27fea67b4ad8d8
clarkdenunzio <clark@clarkdenunzio.com>
2017-08-03 21:26:16 -1000

    handlebars boilerplate secured

--- server.js
+++ server.js
@@ -1,13 +1,34 @@
+const Promise = require('promise');
+
 const express = require('express');
 const exphbs = require('express-handlebars');
+const helpers = require('./lib/helpers.js');
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers: helpers,
+  partialsDir: [
+    'shared/templates/',
+    'views/partials/'
+  ]
+});
+
+app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function(req, res) {
-  res.render('home');
+  res.render('home', {
+    title: 'Home.'
+  });
+});
+
+app.get('/test-page', function(req, res) {
+  res.render('test-page', {
+    title: 'Test.',
+    message: 'I am punctuated'
+  });
 });
 
 app.listen(3000, function(){
