5ca4afcd32245c861ce3325d8d67fcf5cbadfa9d
clarkdenunzio <clark@clarkdenunzio.com>
2017-08-03 18:57:25 -1000

    setting up example

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.listen(3000, function(){
+  console.log('express-handlebars server listening on 3000');
+});
